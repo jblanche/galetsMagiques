6862afca621712415178132f6588fc1844c02acb
Jonathan Blanchet <contact@jblanche.fr>
2014-10-15 18:14:02 +0200

    add tests

---
+++ test/main.js
@@ -0,0 +1,12 @@
+var a = require('../app/javascripts/main.js');
+
+var assert = require("assert");
+
+describe('main', function(){
+	it('is the most basic test you can write', function () {
+		assert.equal(1, 1);
+	});
+
+	it('is a pending test');
+});
+
